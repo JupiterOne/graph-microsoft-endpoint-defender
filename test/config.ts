02b250cc1237d4d89791d773c4d491f5f902e05d
narendra-sacumen <narendrareddy.chitta@sacumen.com>
2022-06-08 19:41:05 +0530

    Folder strcuture changes
    Added the deleted files
    
    Data mapping from machine to device added few properties as per the discussion
    
    Changes done as per PR comments

--- test/config.ts
+++ test/config.ts
@@ -12,10 +12,14 @@ if (process.env.LOAD_ENV) {
 }
 const DEFAULT_CLIENT_ID = 'dummy-acme-client-id';
 const DEFAULT_CLIENT_SECRET = 'dummy-acme-client-secret';
+const DEFAULT_TENANT = 'dummy-tenant';
+const DEFAULT_IS_DEFENDER_API = true;
 
 export const integrationConfig: IntegrationConfig = {
   clientId: process.env.CLIENT_ID || DEFAULT_CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET || DEFAULT_CLIENT_SECRET,
+  tenant: process.env.TENANT || DEFAULT_TENANT,
+  isDefenderApi: DEFAULT_IS_DEFENDER_API,
 };
 
 export function buildStepTestConfigForStep(stepId: string): StepTestConfig {
