4846c89237c307aaf8da69152fb637e678ec81cc
Devika <devika.cb@sacumen.com>
2022-06-14 10:53:51 +0530

    remaining testcases for ms defender

---
+++ src/ms-graph/__tests__/client.test.ts
@@ -0,0 +1,64 @@
+import { createMockIntegrationLogger } from '@jupiterone/integration-sdk-testing';
+
+import { integrationConfig } from '../../../test/config';
+import { GraphClient } from '../client';
+import { IntegrationConfig } from '../../../src/config';
+
+const config: IntegrationConfig = {
+  clientId: 'a8626c1e-191d-4e8f-9cbc-a4a6e85104ac',
+  clientSecret: 'L3X8Q~BO57QkoAsXMSkevrmVR2qiNh.qKEuzucAt',
+  tenant: '5a721b05-53ed-4ed9-be02-aed28f11edbd',
+  isDefenderApi: false,
+};
+const logger = createMockIntegrationLogger();
+
+describe('verifyAuthentication', () => {
+  test('invalid tenant', async () => {
+    const client = new GraphClient(logger, {
+      ...integrationConfig,
+      tenant: 'abc123testing',
+    });
+
+    await expect(client.verifyAuthentication()).rejects.toThrow(
+      'Provider authentication failed at /organization: -1 AuthenticationError',
+    );
+  });
+
+  test('inaccesible', async () => {
+    const client = new GraphClient(logger, integrationConfig);
+
+    await expect(client.verifyAuthentication()).rejects.toThrow(
+      'Provider authentication failed at /organization: 401 InvalidAuthenticationToken',
+    );
+  });
+});
+
+test('fetchMetadata', async () => {
+  const client = new GraphClient(logger, config);
+
+  const metadata = await client.fetchMetadata();
+  console.log(metadata);
+  expect(metadata).toMatchObject({
+    '@odata.context': 'https://graph.microsoft.com/v1.0/$metadata',
+  });
+});
+
+describe('fetchOrganization', () => {
+  test('accessible', async () => {
+    const client = new GraphClient(logger, config);
+
+    const organization = await client.fetchOrganization();
+
+    expect(organization).toMatchObject({
+      displayName: expect.any(String),
+    });
+  });
+
+  test('inaccessible', async () => {
+    const client = new GraphClient(logger, integrationConfig);
+
+    await expect(client.fetchOrganization()).rejects.toThrow(
+      'Provider authorization failed at /organization: 401 InvalidAuthenticationToken',
+    );
+  });
+});
